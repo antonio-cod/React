88540d02dceb3e40442b757dd5049c4c2ac730b7
antonio-cod <antonio_carlostads@hotmail.com>
2024-05-28 15:19:59 -0400

    Componente:Post e Styles do componente

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
-import { Post } from './Post.jsx';
+import { Post } from './components/Post.jsx';
 import { Header } from './components/Header.jsx';
 import { Sidebar } from './components/Sidebar.jsx';
+
 import styles from "./App.module.css";
 
 import './global.css';
---
+++ src/components/Post.jsx
@@ -0,0 +1,32 @@
+import styles from './Post.module.css';
+
+export function Post() {
+    return (
+
+        <article className={styles.post}>
+            <header>
+            <div className={styles.author}>
+                <img className={styles.avatar} src="https://media.licdn.com/dms/image/C4D03AQEb2PamddNalQ/profile-displayphoto-shrink_800_800/0/1660610967599?e=1721865600&v=beta&t=164xG6Yja7FS8Ut2AlZUdmHx5SU0oO-g3X6BzdF46kI" alt="" />
+             <div className={styles.authorInfo}>
+                <strong>Antônio Carlos</strong>
+                <span>Web Developer</span>
+             </div>
+            </div>
+
+            <time title="28 de maio ás 14:39h" dateTime="2024-05-28 14:39:00">Publicado há 1h</time>
+            </header>
+
+        <div className={styles.content}>
+            <p>Fala galeraa</p>
+            <p>Acabei de subir mais um projeto no github</p>
+            <p><a href="">jane.disign/doctorcar</a></p>
+            <p>
+                <a href="">#novoprojeto</a>{' '}
+                <a href="">#nlw</a>{' '}
+                <a href="">#rocketseat</a>
+           </p>
+        </div>
+        </article>
+    )
+
+}
