3b37f67c03a6991faaf94b73f51c767970d40642
antonio-cod <antonio_carlostads@hotmail.com>
2024-05-30 14:42:22 -0400

    3 - Os motores do React / Iterando no JSX

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,38 @@ import styles from "./App.module.css";
 
 import './global.css';
 
+
+const posts = [
+  {
+    id:1,
+    author:{
+      avatarUrl:'https://media.licdn.com/dms/image/C4D03AQEb2PamddNalQ/profile-displayphoto-shrink_800_800/0/1660610967599?e=1721865600&v=beta&t=164xG6Yja7FS8Ut2AlZUdmHx5SU0oO-g3X6BzdF46kI',
+      name:'Antonio Carlos',
+      role: 'Desenvolvedor FrontEnd'
+    },
+    content:[
+      {type: 'paragraph', content: 'Fala galera',},
+      {type: 'paragraph', content: 'Acabei de subir mais um projeto no gitub que fiz na NLW Return'},
+      {type: 'link', content: 'jane.design/doctorcare'},
+    ],
+    publishedAt: new Date('2024-05-30 14:22:00')
+  },
+  {
+    id:2,
+    author:{
+      avatarUrl:'https://media.licdn.com/dms/image/C4D03AQEb2PamddNalQ/profile-displayphoto-shrink_800_800/0/1660610967599?e=1721865600&v=beta&t=164xG6Yja7FS8Ut2AlZUdmHx5SU0oO-g3X6BzdF46kI',
+      name:'Mayk Brito',
+      role: 'Educador @Rocketseat'
+    },
+    content:[
+      {type: 'paragraph', content: 'Fala galera',},
+      {type: 'paragraph', content: 'Acabei de subir mais um projeto no gitub que fiz na NLW Return'},
+      {type: 'link', content: 'jane.design/doctorcare'},
+    ],
+    publishedAt: new Date('2024-05-30 14:22:00')
+  },
+];
+
 export function App() {
   return (
     <div>
@@ -14,20 +46,18 @@ export function App() {
     <div className={styles.wrapper}>
       <Sidebar />
       <main>
-      <Post 
-     author='Antonio Carlos'
-     content="Lorm ipum dolor sit content is world"
-     />
-
-    <Post 
-     author='Carlos Cruz'
-     content="Lorem ipum dolor"
-     />
+      {posts.map(post => {
+        return (
+        <Post 
+        author={post.author}
+        content={post.content}
+        publishedAt={post.publishedAt}
+        />
+        )
+      })}
       </main>
-
     </div>
-
-    </div>
+  </div>
   
   )
 }
