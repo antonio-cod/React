9378e54e3896b0d9b75366bb98a2e91c04524092
antonio-cod <antonio_carlostads@hotmail.com>
2024-05-31 14:14:41 -0400

    Estado(useState)

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -5,7 +5,15 @@ import { Avatar } from './Avatar';
 import { Comment } from './Comment';
 import styles from './Post.module.css';
 
+import { useState } from 'react';
+
+
 export function Post({ author, publishedAt, content }) {
+    const [comments, setComments] = useState([
+        1,
+        2,
+    ])
+    
     const publishedDateFormatted = format(publishedAt, "d 'de' LLLL 'ás' HH:mm'h'", {
         locale: ptBR,
     })
@@ -14,7 +22,13 @@ export function Post({ author, publishedAt, content }) {
         locale: ptBR,
         addSuffix: true,
     })
-       
+    
+    function handleCreateNewComment() {
+        event.preventDefault()
+
+        setComments([...comments, comments.length + 1]);
+    }
+
     return (
         <article className={styles.post}>
             <header>
@@ -42,21 +56,22 @@ export function Post({ author, publishedAt, content }) {
             
         </div>
 
-        <form className={styles.commentForm}>
+        <form onSubmit={handleCreateNewComment} className={styles.commentForm}>
             <strong>Deixe seu feedback</strong>
 
             <textarea
              placeholder='Deixe um comentário'
             />
+
             <footer>
             <button type="submit">Publicar</button>
             </footer>
         </form>
 
         <div className={styles.commentList}>
-            <Comment />
-            <Comment />
-            <Comment />
+            {comments.map(comment => {
+                return <Comment />
+            })}
         </div>
 
         </article>
